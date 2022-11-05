97cad893a8c02fefac6d945cddd38fc2d4e215b7
satpathysarthak <satpathysarthak@gmail.com>
2022-11-05 19:27:03 -0400

    Update1926

--- shell_commands/20210312_KM.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while read -r line; do
-	# Reading each line
-	echo $line
-	Rscript /home/workstation/Documents/Sarthak/gender_prognosis/Rcodes/20201025_KM_plots-copy.R $line 
-done < /home/workstation/Documents/Sarthak/gender_prognosis/analysis/trial.txt
-#done < /home/workstation/Documents/Sarthak/gender_prognosis/analysis/max_min_reg.txt
--- shell_commands/run1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-date
-for i in {1..20}
-do
-   echo "$i"
-   Rscript /home/workstation/Documents/Sarthak/gender_prognosis/Rcodes/20210626_model.R HNSC $i 1>>/home/workstation/Documents/Sarthak/gender_prognosis/Rcodes/log/20210626_model.log.out 2>> /home/workstation/Documents/Sarthak/gender_prognosis/Rcodes/log/20210626_model.log.err
-   date
-done
